92d2e908f1fc6d5553537772fa05091eb71dabd1
MestreLion <linux@rodrigosilva.com>
2023-04-06 18:36:57 -0300

    2023 update

--- script.py
+++ script.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program. See <http://www.gnu.org/licenses/gpl.html>
 
-# This file is part of [PROJNAME], see <https://github.com/MestreLion/[PROJTAG]>
+# This file is part of [PROJECT_NAME], see <https://github.com/MestreLion/[PROJECT_SLUG]>
 # Copyright (C) [YEAR] Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
 # License: GPLv3 or later, at your choice. See <http://www.gnu.org/licenses/gpl>
 
@@ -27,10 +27,11 @@ import argparse
 import logging
 import os
 import sys
+import typing as t
 
 
-COPYRIGHT="""
-Copyright (C) 2018 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
+COPYRIGHT = """
+Copyright (C) 2023 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
 License: GPLv3 or later, at your choice. See <http://www.gnu.org/licenses/gpl>
 """
 
@@ -38,14 +39,19 @@ log = logging.getLogger(os.path.basename(os.path.splitext(__file__)[0]))
 
 
 class ProjectError(Exception):
-    """Base class for custom exceptions, with errno and %-formatting for args.
+    """Base class for custom exceptions with a few extras on top of Exception.
+
+    - %-formatting for args, similar to logging.log()
+    - `errno` numeric attribute, similar to OSError
+    - `e` attribute for the original exception, when re-raising exceptions
 
     All modules in this package raise this (or a subclass) for all
-    explicitely raised, business-logic, expected or handled exceptions
+    explicitly raised, business-logic, expected or handled exceptions
     """
-    def __init__(self, msg:str="", *args, errno:int=0):
-        super().__init__(str(msg) % args)
+    def __init__(self, msg: object = "", *args, errno: int = 0, e: Exception = None):
+        super().__init__((str(msg) % args) if args else msg)
         self.errno = errno
+        self.e = e
 
 
 def parse_args(argv=None):
@@ -72,7 +78,7 @@ def parse_args(argv=None):
     parser.add_argument('-a', '--arg',
                         default="somearg",
                         help="Some Arg."
-                            " [Default: %(default)s]")
+                             " [Default: %(default)s]")
 
     parser.add_argument('-o', '--option',
                         dest='option',
@@ -90,8 +96,14 @@ def parse_args(argv=None):
     return args
 
 
-def main(argv=None):
+def main(argv: t.Optional[t.List[str]] = None):
     args = parse_args(argv)
+    logging.basicConfig(
+        level=args.loglevel,
+        format="[%(asctime)s %(name)-6s %(levelname)-6.6s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
     logging.basicConfig(level=args.loglevel,
                         format='%(levelname)-5.5s: %(message)s')
     log.debug(args)
@@ -109,8 +121,10 @@ if __name__ == '__main__':
         # https://docs.python.org/3/library/signal.html#note-on-sigpipe
         devnull = os.open(os.devnull, os.O_WRONLY)
         os.dup2(devnull, sys.stdout.fileno())
-    except Exception as e:
-        log.critical(e, exc_info=True)
+    except ProjectError as err:
+        log.error(err)
+    except Exception as err:
+        log.exception(err)
         sys.exit(1)
     except KeyboardInterrupt:
         log.info("Aborting")
